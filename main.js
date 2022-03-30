85bd76b8493313fcd429067b34d92b24dc325afe
Deltalay <paylayhok168@gmail.com>
2022-03-30 21:02:31 +0700

    Update limit

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-const Uglify = require('uglify-js');
-const data = require('./mini/index')
-var cors = require('cors')
-const express = require('express');
+const Uglify = require("uglify-js");
+const data = require("./mini/index");
+var cors = require("cors");
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json({ limit: "50mb", }));
 app.use(express.static(__dirname + "/views"));
-app.get('/', (req,res) => {
-  res.render('index.ejs')
+app.get("/", (req, res) => {
+  res.render("index.ejs");
 });
-app.use('/v1/result', data)
+app.use("/v1/result", data);
 app.listen(PORT, () => {
-  console.log(`Server online on Port: ${PORT}`)
+  console.log(`Server online on Port: ${PORT}`);
 });
